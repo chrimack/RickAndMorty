ae2cb9739861251ec0967e6b837f5b13e5ae17a2
Chris McClelland <clm727@gmail.com>
2021-06-20 17:15:20 -0400

    add locations to state

--- client/src/Episodes.jsx
+++ client/src/Episodes.jsx
@@ -1,6 +1,21 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 const Episodes = () => {
+  const [episodes, setEpisodes] = useState([]);
+  const [nextPage, setNextPage] = useState('');
+
+  const getEpisodes = () => {
+    axios.get('/episode')
+      .then(res => {
+        setEpisodes(res.data.results);
+        setNextPage(res.data.info.next);
+      });
+  };
+
+  useEffect(() => {
+    getEpisodes();
+  }, []);
 
   return (
     <div></div>
