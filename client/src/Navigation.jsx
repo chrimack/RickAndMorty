0c1f1152abed8562857ffd728ce967389ad26d88
Chris McClelland <clm727@gmail.com>
2021-06-21 08:40:51 -0400

    add css reset

---
+++ client/src/Navigation.jsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import Home from './Home.jsx';
+import Characters from './Characters.jsx';
+import Locations from './Locations.jsx';
+import Episodes from './Episodes.jsx';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from 'react-router-dom';
+
+const Navigation = () => {
+
+  return (
+    <Router>
+      <div>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/characters">Characters</Link>
+          </li>
+          <li>
+            <Link to="/locations">Locations</Link>
+          </li>
+          <li>
+            <Link to="/episodes">Episodes</Link>
+          </li>
+        </ul>
+        <hr />
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route exact path="/characters">
+            <Characters />
+          </Route>
+          <Route exact path="/locations">
+            <Locations />
+          </Route>
+          <Route export path="/episodes">
+            <Episodes />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
+};
+
+export default Navigation;
