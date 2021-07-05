91ac704b8a9e09e27165ab51dd62036968e0301d
Chris McClelland <clm727@gmail.com>
2021-07-05 13:05:53 -0400

    gitignore

--- README.md
+++
@@ -1,2 +0,0 @@
-# RickAndMorty
-Rick and Morty wiki
--- client/src/components/home/Trailer.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import axios from 'axios';
-
-const Trailer = () => {
-  return <></>;
-};
-
-export default Trailer;
